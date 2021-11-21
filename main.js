eaf12797dede291afa191c6058b9cceb1708cad4
Matheus Prado Agostinho <mat.matheus_pa@hotmail.com>
2021-11-21 16:36:08 -0300

    update page, news features

---
+++ main.js
@@ -0,0 +1,40 @@
+let spanNumber = document.getElementById('number');
+let count = 5; //contador
+
+function changeNumber(){ // Altera número no contator
+  spanNumber.innerHTML = `${count}`;
+}
+
+function redirect(){ // Redirecionamento
+  window.location.href = "https://github.com/TheMath123";
+}
+
+changeNumber();
+
+// Temporizador bem improvisado
+setTimeout(()=>{
+  console.log(count);
+  changeNumber();
+  count--;
+}, 1000);
+setTimeout(()=>{
+  changeNumber();
+  count--;
+}, 2000);
+setTimeout(()=>{
+  changeNumber();
+  count--;
+}, 3000);
+setTimeout(()=>{
+  changeNumber();
+  count--;
+}, 4000);
+setTimeout(()=>{
+  changeNumber();
+  count--;
+}, 5000);
+setTimeout(()=>{
+  changeNumber();
+  count--;
+  // redirect();
+}, 6000);
