19284041815c41ec72cf37b7e235fbab81611cbb
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2022-03-07 10:58:01 -0400

    design bar

--- src/components/taskbar/index.tsx
+++ src/components/taskbar/index.tsx
@@ -1,18 +1,34 @@
-import Link from 'next/link'
-import styles from './taskbar.module.scss'
+import Image from "next/image";
+import styles from "./taskbar.module.scss";
 
-export function Taskbar(){
-  return(
+export function Taskbar() {
+  return (
     <div className={styles.taskbar}>
       <div className={styles.icons}>
-        <button>Start</button>
-        <Link href="#">HiText</Link>
-        <Link href="#">StartMessage</Link>
+        <button>
+          <Image src="/door.svg" alt="Door" width={27} height={27} />
+        </button>
+        <button onClick={() => window.open("https://hitext.vercel.app/")}>
+          <Image
+            src="/hitext.svg"
+            alt="Aplicação do HiText"
+            width={27}
+            height={27}
+          />
+        </button>
+        <button onClick={() => window.open("https://startmessage.vercel.app/")}>
+          <Image
+            src="/startmessage.png"
+            alt="Aplicação do StartMessage"
+            width={27}
+            height={27}
+          />
+        </button>
       </div>
       <div className={styles.info}>
         <span>21:51</span>
         <span>03/03/2022</span>
       </div>
     </div>
-  )
+  );
 }
