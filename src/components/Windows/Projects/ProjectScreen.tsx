90fe42c27fa29081ca0b716ad8fac2eae1c6e9e1
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2022-04-13 12:28:48 -0400

    starting project integration

--- src/components/Windows/Projects/ProjectScreen.tsx
+++ src/components/Windows/Projects/ProjectScreen.tsx
@@ -1,49 +1,29 @@
+import { useEffect, useState } from "react";
 import { ProjectItem } from "./ProjectItem";
 import { Window } from "../window";
 
 export function ProjectScreen() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    setProjects("../../../assets/projects.json");
+  });
+
   return (
     <Window name="Projetos" windowIndex={1}>
-      <ProjectItem
-        title="Start Message"
-        description="Gerador um link para iniciar um conversa no WhatsApp a partir de um número de telefone."
-        image={{
-          alt:"Logo da Aplicação do StartMessage, um bolãozinho de conversa verde com telefone na cor branco dentro.",
-          url:"https://i.imgur.com/0dxg2EZ.png"
-        }}
-        repository={{
-          demoURL:"https://startmessage.vercel.app/",
-          projectURL:"https://github.com/TheMath123/StartMessage",
-        }}
-        techs={[
-          {
-            color:'js',
-            title:'Javascript',
-          },
-          {
-            color:'html',
-            title:'HTML',
-          },
-          {
-            color:'css',
-            title:'CSS',
-          },
-        ]}
-        concepts={[
-          {
-            color:'ux',
-            title:'UX'
-          },
-          {
-            color:'mobile',
-            title:'Mobile First'
-          },
-          {
-            color:'responsiveness',
-            title:'Responsividade'
-          },
-        ]}
-      />
+      {projects.map((item, index) => {
+        return (
+          <ProjectItem
+            key={index}
+            title={item.title}
+            description={item.description}
+            image={item.image}
+            repository={item.repository}
+            techs={item.techs}
+            concepts={item.concepts}
+          />
+        );
+      })}
     </Window>
   );
 }
