924449d0d27faada183c04423538190403ae8f16
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2022-04-14 08:57:46 -0400

    resolved project listing issue, and responsiveness issues

--- src/components/Windows/Projects/ProjectScreen.tsx
+++ src/components/Windows/Projects/ProjectScreen.tsx
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react";
 import { ProjectItem } from "./ProjectItem";
 import { Window } from "../window";
+import projectsList from "../../../assets/projects.json";
+import styles from "./ProjectScreen.module.scss";
 
 export function ProjectScreen() {
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    // setProjects("../../../assets/projects.json");
-  });
+    console.log(projectsList);
+    setProjects(projectsList);
+  }, []);
 
   return (
     <Window name="Projetos" windowIndex={1}>
       {projects.map((item, index) => {
         return (
-          <ProjectItem
-            key={index}
-            title={item.title}
-            description={item.description}
-            image={item.image}
-            repository={item.repository}
-            techs={item.techs}
-            concepts={item.concepts}
-          />
+          <div className={styles.projectBox}>
+            <ProjectItem
+              key={index}
+              title={item.title}
+              description={item.description}
+              image={item.image}
+              repository={item.repository}
+              techs={item.techs}
+              concepts={item.concepts}
+            />
+            <div className={styles.partition}></div>
+          </div>
         );
       })}
     </Window>
