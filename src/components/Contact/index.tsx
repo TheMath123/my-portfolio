826242c38c038b1502b864d4d3a8f410cd0ac09f
Matheus P Agostinho <mat.matheus_pa@hotmail.com>
2022-04-20 12:17:42 -0400

    fixed font

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Anchor } from "../Anchor";
-import styles from "./Contact.module.scss";
+import styles from "./contact.module.scss";
 
 type ContactProps = {
   title: string;
@@ -12,14 +12,21 @@ type ContactProps = {
 
 export function Contact(props: ContactProps) {
   return (
-    <main className={styles.container}>
-      <Image src={props.imageUrl} alt={props.imageAlt} height={34} width={34} />
+    <div className={styles.main}>
+      <div className={styles.images}>
+        <Image
+          src={props.imageUrl}
+          alt={props.imageAlt}
+          height={34}
+          width={34}
+        />
+      </div>
       <div className={styles.infos}>
         <h1>{props.title}</h1>
         <Anchor url={props.url} title={props.description}>
           {props.description}
         </Anchor>
       </div>
-    </main>
+    </div>
   );
 }
