540e00093cc30ec71687d985a79878f16fa5476c
Matheus P Agostinho <contact@matheuspa.me>
2022-11-23 17:36:15 -0300

    fix: changes url images, for local

--- src/pages/TalkToMe/TalkToMeScreen.tsx
+++ src/pages/TalkToMe/TalkToMeScreen.tsx
@@ -11,7 +11,7 @@ export default function TalkToMeScreen() {
           title="E-mail"
           description="math.p.agostinho@outlook.com"
           url="mailto:math.p.agostinho@outlook.com"
-          imageUrl="https://firebasestorage.googleapis.com/v0/b/matheuspa-projects.appspot.com/o/my-portfolio%2Ficons%2Fmail.png?alt=media&token=0341b64a-51b9-45d9-bd50-8df459f5d1de"
+          imageUrl="/icons/contacts/mail.png"
           imageAlt="Envelope na cor azul"
         />
 
@@ -19,8 +19,8 @@ export default function TalkToMeScreen() {
           title="Linkedin"
           description="matheus-p-agostinho"
           url="https://www.linkedin.com/in/matheus-p-agostinho/"
-          imageUrl="https://firebasestorage.googleapis.com/v0/b/matheuspa-projects.appspot.com/o/my-portfolio%2Ficons%2Flinkedin.png?alt=media&token=465e0c72-05d4-4d63-b7f0-51ebc11916a3"
-          imageAlt="Quadrado com borda arrendondada na cor azul,
+          imageUrl="/icons/contacts/linkedin.ong"
+          imageAlt="Logo da rede social Linkedin, um quadrado com borda arrendondada na cor azul,
           com letras i n em cor branca no meio"
         />
 
@@ -28,7 +28,7 @@ export default function TalkToMeScreen() {
           title="Github"
           description="TheMath123"
           url="https://github.com/TheMath123"
-          imageUrl="https://firebasestorage.googleapis.com/v0/b/matheuspa-projects.appspot.com/o/my-portfolio%2Ficons%2Fgithub.png?alt=media&token=bb96b166-f48f-41fe-b908-7b7657f08cec"
+          imageUrl="/icons/contacts/github.png"
           imageAlt="Circulo na cor azul arroxeado
           com gato polvo simbolo do github dentro na cor do fundo"
         />
