b8ef419d65ec9d3953a3064e870361fe0e8d9547
Matheus P Agostinho <contact@matheuspa.me>
2023-03-15 12:18:46 -0300

    feat: redesign 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,26 +1,72 @@
 import { useEffect, useState } from "react";
+import styles from "../styles/404.module.scss";
 
 export default function Custom404() {
-  const [isMobile, setIsMobile] = useState(false);
+  const [restartPercentage, setRestartPercentage] = useState(0);
+  let checkDevice = false;
 
   useEffect(() => {
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(
-      window.navigator.userAgent,
+    checkDevice = /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent);
+  }, []);
+
+  useEffect(() => {
+    let restartingTimer = setTimeout(
+      () => {
+        checkAndRedirect(checkDevice);
+        changePercetageRestarting();
+      },
+      restartPercentage < 50 ? 1000 : 2000,
     );
+    // return clearTimeout(restartingTimer);
+  }, [restartPercentage]);
 
-    if (isMobile) {
-      setIsMobile(isMobile);
+  const changePercetageRestarting = () => {
+    if (restartPercentage < 0 || restartPercentage >= 100) return;
 
+    setRestartPercentage(restartPercentage + 10);
+  };
+
+  const checkAndRedirect = (isMobile: boolean) => {
+    if (isMobile && restartPercentage >= 100) {
       redirectTo("https://m.matheuspa.me/");
     } else {
       redirectTo("https://matheuspa.me/");
     }
     return;
-  }, []);
+  };
 
   const redirectTo = (url: string) => {
     window.location.href = url;
   };
 
-  return <h1>404 - Page Not Found</h1>;
+  return (
+    <main className={styles.container}>
+      <div className={styles.content}>
+        <section className={styles.titleBox}>
+          <h1 className={styles.emojiFace}>:(</h1>
+          <h2 className={styles.title}>
+            It looks like the page you are looking for does not exist.
+          </h2>
+          <span className={styles.percentRestart}>
+            Restarting on {restartPercentage}%
+          </span>
+        </section>
+        <section className={styles.erroBox}>
+          <img
+            src="images\qr-code-website.png"
+            alt="QR Code"
+            className={styles.qrCode}
+          />
+          <div className={styles.msgError}>
+            <h1>
+              System will restart and it will take you to a really useful page.
+            </h1>
+            <p>
+              Error Code: <span>404 - Not found page</span>
+            </p>
+          </div>
+        </section>
+      </div>
+    </main>
+  );
 }
